707b84989676fc48fb7ba85f7858ef348ece6554
Alejandro Lara <dany.11082011@gmail.com>
2025-07-26 21:27:03 -0600

    primer commit, tipos de variables

---
+++ IntroduccionJava/src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variables
+        byte edad = 28;
+        double precio = 850.50;
+        boolean disponible = true;
+        char genero;
+        genero = 'M'; //MASCULINO -M, FEMENINO -F
+
+        //accede a las variables
+        System.out.println(edad);
+        edad += 1;
+        System.out.println(edad);
+
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+    }
+}
