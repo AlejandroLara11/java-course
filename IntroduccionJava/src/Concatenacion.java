b85400f401d91dc82c3c07be800bf06104edb2bc
Alejandro Lara <dany.11082011@gmail.com>
2025-07-30 23:55:04 -0600

    TipoVar, concatenacion y constantes en Java

---
+++ IntroduccionJava/src/Concatenacion.java
@@ -0,0 +1,9 @@
+public class Concatenacion {
+    public static void main(String[] args) {
+        System.out.println("*** CONCATENCACION DE CADENAS. *** \n");
+        var nombre = "Alejandro";
+        var apellido = "Lara";
+        var nombreCompleto = nombre + " " + apellido;
+        System.out.println("Nombre Completo: " + nombreCompleto);
+    }
+}
