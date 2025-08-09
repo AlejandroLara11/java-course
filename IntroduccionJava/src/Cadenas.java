e58c74ed65643a35f08a76db627ea72ec7d8e515
Alejandro Lara <dany.11082011@gmail.com>
2025-08-09 15:25:00 -0600

    Cadenas en Java

---
+++ IntroduccionJava/src/Cadenas.java
@@ -0,0 +1,25 @@
+public class Cadenas {
+    public static void main(String[] args){
+        var cadenaMultilinea =
+                """
+                con triple comilla se puede
+                generar una cadena
+                multilinea
+                """;
+        System.out.println(cadenaMultilinea);
+
+        var cadena1 = new String("Manera formal de definir una cadena");
+        System.out.println(cadena1);
+
+        //Recuperar un caracter mediante indices
+        var primerCaracter = cadena1.charAt(0);
+        System.out.println("Primer caracter: " + primerCaracter);
+
+        var ultimoCaracter = cadena1.charAt(cadena1.length() - 1);
+        System.out.println("Ultimo caracter: " + ultimoCaracter);
+
+        var largoCadena = cadena1.length();
+        System.out.println("Largo cadena: " + largoCadena);
+
+    }
+}
---
+++ IntroduccionJava/src/ComparacionYPoolCadenas.java
@@ -0,0 +1,10 @@
+public class ComparacionYPoolCadenas {
+    public static void main(String[] args) {
+        var cadena1 = "Hola";
+        var cadena2 = "Hola";
+
+        System.out.print("La cadena 1 es igual a la cadena 2: ");
+        System.out.println(cadena1 == cadena2);
+
+    }
+}
