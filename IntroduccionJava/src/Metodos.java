1dd9edfab1710f2e207f5c198947e221bab7c8bb
Alejandro Lara <dany.11082011@gmail.com>
2025-08-23 17:05:26 -0600

    Metodos simples

---
+++ IntroduccionJava/src/Metodos.java
@@ -0,0 +1,25 @@
+public class Metodos {
+
+    static int sumar(int a, int b){
+        var res = a+b;
+        return res;
+    }
+
+    static void saludar(String mensaje){
+        System.out.println("Hola " + mensaje + ", Bienvenido.");
+    }
+
+    static boolean esPar(int num){
+        return (num%2==0)? true : false;
+    }
+
+    public static void main(String[] args) {
+        saludar("Alejandro");
+
+        var resultado = sumar(2,5);
+        System.out.println(resultado);
+
+        var salida = esPar(4);
+        System.out.println(salida);
+    }
+}
