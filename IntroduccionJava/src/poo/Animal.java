ee4d0f60a3c91fcbe59432016ecd5c88ce4ceb4e
Alejandro Lara <dany.11082011@gmail.com>
2025-08-26 15:53:07 -0600

    Herencia y sobreescritura de metodos

--- IntroduccionJava/src/poo/Animal.java
+++ IntroduccionJava/src/poo/Animal.java
@@ -2,11 +2,11 @@ package poo;
 
 public class Animal {
 
-    public void Comer(){
+    public void comer(){
         System.out.println("Comiendo...");
     }
 
-    public void Dormir(){
+    public void dormir(){
         System.out.println("Durmiendo...");
     }
 }
@@ -15,19 +15,24 @@ class Perro extends Animal{
     public void hacerSonido(){
         System.out.println("Ladrando...");
     }
+
+    @Override
+    public void dormir(){
+        System.out.println("Durmiendo 10 horas al dia...");
+    }
 }
 
 class PruebaAnimal{
     public static void main(String[] args) {
         var animalPrueba  = new Animal();
         System.out.println("Realizando prueba con ANIMAL...");
-        animalPrueba.Comer();
-        animalPrueba.Dormir();
+        animalPrueba.comer();
+        animalPrueba.dormir();
 
         var perroPrueba = new Perro();
         System.out.println("Realizando prueba con PERRO...");
-        perroPrueba.Comer();
-        perroPrueba.Dormir();
+        perroPrueba.comer();
+        perroPrueba.dormir();
         perroPrueba.hacerSonido();
 
     }
