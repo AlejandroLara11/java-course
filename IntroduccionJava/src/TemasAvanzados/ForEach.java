113c9033000e2eb0fe8eb4806822fe204e51f9fe
Alejandro Lara <dany.11082011@gmail.com>
2025-08-29 20:50:55 -0600

    Uso del For-Each_1

--- IntroduccionJava/src/TemasAvanzados/ForEach.java
+++ IntroduccionJava/src/TemasAvanzados/ForEach.java
@@ -1,4 +1,10 @@
 package TemasAvanzados;
 
 public class ForEach {
+    public static void main(String[] args) {
+        int edades[] = {21, 23, 25, 28};
+        for(int numero : edades){ //EN ESTE TIPO DE FOR, NO TENEMOS ACCESO AL INDICE
+            System.out.println("Edad: "+numero);
+        }
+    }
 }
